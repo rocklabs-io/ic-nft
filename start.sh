d45bef3c4920c413d08c978b30e050db16bb469c
XUSD2 <hanyuhui@xinye.com>
2021-08-07 01:27:31 +0800

    fix upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+dfx stop
+rm -rf .dfx
+
+ALICE_HOME=$(mktemp -d -t alice-temp)
+
+ALICE_PUBLIC_KEY="principal \"$( \
+    HOME=$ALICE_HOME dfx identity get-principal
+)\""
+
+dfx start --background
+dfx canister --no-wallet create token_ERC721
+dfx build
+
+dfx canister --no-wallet install token_ERC721 --argument="(\"Test NFT 1\", \"NFT1\",$ALICE_PUBLIC_KEY)" -m=reinstall
